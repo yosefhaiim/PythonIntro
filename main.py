d0ddec335d388eb193363c9555f1c2aa794799bd
Yosef Zeegdon <yhz@orsameach.com>
2024-09-08 15:49:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+# import this
+#from tkinter.font import names
+
+# name = "Yossef"
+# print("line 1")
+# print("line 2")
+#
+# print("Hi, my name is", end="")
+# print(name)
+
+
+# first_name="Yossef"
+# last_name = "zeegdon"
+# age= 21
+# ziun = 100
+# print(f"{first_name} {last_name} {age} {ziun}")
+#
+# print(f"{first_name}\t{last_name}\t{age}\t{ziun}")
+#
+# print(f"{first_name} {last_name}\n{age}\n{ziun}")
+#
+# str1 = "I ikkjjk kikl,mm ;;lkmmmlxk  jjkk"
+# str2 = "rtyuio fghj cvbn edc polkmn kkk"
+# str3 = "tryuio kikjnjnm jkl, kkmm,lkl ddghnn"
+#
+# print(str1[5:15:4])
+# print(str2[-1: -6])
+# print(str3[10::2])
+#
+#
+# name = "Yossef"
+# age = 25
+# print(f"{name} {age}")
+#
+# x = 10
+# y = 20
+# sum = x + y
+# print(sum)
+#
+# first_name = "Yossef"
+# last_name = "zeegdon"
+# full_name = first_name + " " + last_name
+# print(full_name)
+#
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+# print(f"Hello you are {user_name} and your age is {user_age}")
+#
+# number_string = "50"
+# numer = int(number_string)
+# print(numer)
+#
+#
+# calc_age = int(input("Enter your age: "))
+# print(f"you are {str(2023 - calc_age)} years in 2023 ")
+#
+# age = 30
+#
+# print("Hello world")
+#
+# name = input("Enter your name: ")
+# print(f"Hello {name}")
+#
+# python = "Python"
+# print(python.upper())
+#
+# print(len("Program")) #7
+#
+# print(f"{name} and I am {age} years old.")
+#
+# temperature = 35.5
+# print(f"the now is {temperature} degrees Celsius.")
+#
+# enter_name = input("Enter your name: ")
+# print(len(enter_name))
+#
+#
+# full_name = f"{first_name} + {last_name}"
+
+# num1 = 99
+# num = 122.9739900
+# print(f"{num:.2f}")
+# print(f"{num:,}")
+# לבדוק מה זה עושה
+# print(f"{num1:06d}")
+
+# fruits = ["apple", "pear", "orange", "banana", "strawberry"]
+# for i in fruits:
+#     print(f"fruit is {i}")
+#
+# for i, j  in enumerate( fruits):
+#     print(f"fruit {i} is in index {j}")
+#
+# numbers = [1, 3, 6, 7, 8,9, 6, 8, 10, 89]
+# num =0
+# for i in numbers:
+#     num += i
+# print(num // len(numbers))
+#
+# sum_numbers = sum(numbers)
+# print(sum_numbers/ len(numbers))
+#
+#
+# maxi = numbers[0]
+# for i in numbers:
+#     if i > maxi:
+#         maxi = i
+# print(maxi)
+#
+# print(max(numbers))
+#
+# numbers.sort()
+# print(numbers[-1])
+#
+#
+# str1 = "poiu fghj hjk klkmj  ol, ;"
+# vowels = "aeiou"
+# count = 0
+#
+# for car in str1:
+#     if car in vowels:
+#         count += 1
+# print(count)
+
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     elif i % 2 != 0:
+#         print(i)
+#     else:
+#         continue
+
+# for i in  range(2, 50000):
+#     is_prime = True
+#     for j in range(2, int(i ** 0.5) + 1):
+#         if i % j == 0:
+#             is_prime = False
+#             break
+#     if is_prime:
+#         print(str(i) + "is a prime number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
